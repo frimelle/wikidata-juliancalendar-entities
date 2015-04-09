86284e66f0aeb11ab7d908b478d63bbb8e8a76e9
lucie <lucie.kaffee@wikimedia.de>
2015-04-09 19:15:42 +0200

    Get all entities and their changes, if the changes were done before 1. May 2014

--- getNewItems.py
+++ getNewItems.py
@@ -3,9 +3,11 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import csv
 from collections import defaultdict
+import urllib2
+import json
 
 dates_query = """
-SELECT ?entity ?timeString
+SELECT ?entity ?property ?timeString
 WHERE {
   ?entity ?property ?valueNode .
   ?valueNode <http://www.wikidata.org/ontology#preferredCalendar>
@@ -13,6 +15,18 @@ WHERE {
   ?valueNode <http://www.wikidata.org/ontology#time> ?timeString .
 }
 """
+
+date = '2014-05-01'
+
+sparqlDict = {}
+
+def url_opener(url):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Script to get the statistics, lucie.kaffee@wikimedia.de')] 
+    infile = opener.open(url)
+    page = json.load(infile)
+    return page
+
 def queryData( sparql_query ):
     try:
         sparql = SPARQLWrapper( "http://milenio.dcc.uchile.cl/sparql" )
@@ -28,12 +42,39 @@ def getSPARQLdata():
     results = queryData( dates_query )
     #write results in List
     for result in results["results"]["bindings"]:
-        if '<http://www.w3.org/2001/XMLSchema#gYear>' in result["timeString"]['value']:
-            continue
-        entity_id = result["entity"]['value'].split('S')[0].split('entity/')[1]
-        sparqlList.append( entity_id )
-    print len( sparqlList )
+        if not '<http://www.w3.org/2001/XMLSchema#gYear>' in result["timeString"]['value']:
+        
+            entity_id = result["entity"]['value'].split('S')[0].split('entity/')[1]
+            prop = result["property"]['value'].split('q')[0].split('entity/')[1]
+            if 'r' in prop:
+                prop = prop.split('r')[0]
+            if 'v' in prop:
+                prop = prop.split('v')[0]
+
+            if checkTime(entity_id, prop): 
+
+                sparqlList.append( entity_id )
+
+    print "SPARQL list: " + str(len( sparqlList ))
     return sparqlList
+
+### check if items are edited before a certain date
+def checkTime(entity_id, prop):
+    page = url_opener("https://www.wikidata.org/w/api.php?action=query&prop=revisions&titles=" + entity_id + "&rvlimit=500&rvprop=timestamp|user|comment|ids&indexpageids=1&format=json")
+    pageids = page['query']['pageids']
+    for pageid in pageids: 
+        if 'revisions' in page['query']['pages'][pageid]:
+            for revision in page['query']['pages'][pageid]['revisions']:
+                if prop in revision['comment']:
+                    if not 'wbsetreference' in revision['comment']:
+                        if revision['timestamp'] < date:
+                            if not entity_id in sparqlDict:
+                                sparqlDict[entity_id] = 'http://www.wikidata.org/w/index.php?title=' + entity_id + '&diff=' + str(revision['revid'])
+                            else: 
+                                sparqlDict[entity_id] = 'http://www.wikidata.org/w/index.php?title=' + entity_id + '&diff=' + str(revision['revid']) + " " + str(sparqlDict[entity_id])
+                            return True
+            
+
 
 def getCSVdata( csvfile ):
     columns = defaultdict( list ) # each value in each column is appended to a list
@@ -47,7 +88,7 @@ def getCSVdata( csvfile ):
                                      # based on column name k
 
     data = columns['entity_id']
-    print len( data )
+    print "CSV list: " + str(len( data ))
     return data
 
 def writeFile(file, list):
@@ -56,15 +97,21 @@ def writeFile(file, list):
          f.write('http://wikidata.org/wiki/' + item + '\n')
      f.close()
 
-### this part is to compare with a csv file, commented out to make it testable
-'''
+### this part is to compare with a csv file
 def compareLists():
     sparqlList = getSPARQLdata()
     csvList = getCSVdata( 'JulianCalendarChanges.csv' )
     newItems = list( set( sparqlList ) - set( csvList ) )
-    len( newItems )
+    print "New list: " + str(len( newItems ))
     return newItems
 
-writeFile('newItems.txt', compareLists())
-'''
-writeFile('newItems.txt', getSPARQLdata())
+def createItemPropList():
+    comparedLists = compareLists()
+    resultList = []
+    for c in comparedLists:
+        resultList.append(c + " " + sparqlDict.get(c))
+    return resultList
+
+writeFile('newItems-prop-new.txt', createItemPropList())
+
+#writeFile('newItems.txt', getSPARQLdata())
--- getRandomItem.py
+++ getRandomItem.py
@@ -3,16 +3,16 @@ import os
 
 content = []
 
-orig_file = 'newItems.txt'
-working_file = 'newItems.txt~'
+orig_file = 'newItems-prop-new.txt'
+working_file = 'newItems-prop.txt~'
 
 with open( orig_file ) as f, open( working_file, 'w+' ) as working:
     content = f.readlines()
-    entity_url = random.choice( content ).strip( '\n' )
-    print entity_url
+    urls = random.choice( content ).strip( '\n' )
+    print urls
 
     for line in content:
-        if entity_url not in line:
+        if urls not in line:
             working.write( line )
 
 os.rename( working_file, orig_file )
